2048c50d361715ef9fed7a905935eb7b3b476685
Andrew Hughes <andrewcarterhughes@gmail.com>
2018-09-26 13:54:03 -0700

    added intial round of files

--- src/main/java/com/okta/springbootwithauth/SimpleAppController.java
+++ src/main/java/com/okta/springbootwithauth/SimpleAppController.java
@@ -1,8 +1,5 @@
-package com.okta.springboot.simpleapp;
+package com.okta.springbootwithauth;
 
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -12,17 +9,6 @@ class SimpleAppController {
     @RequestMapping("/")
     String home() {
         return "home";
-    }
-
-    @RequestMapping("/restricted")
-    String restricted() {
-        return "restricted";
-    }
-
-    @RequestMapping("/admin")
-    @PreAuthorize("hasAuthority('Admin')")
-    String admin() {
-        return "admin";
     }
 
 }
--- src/main/java/com/okta/springbootwithauth/SpringSecurityWebAppConfig.java
+++ src/main/java/com/okta/springbootwithauth/SpringSecurityWebAppConfig.java
@@ -1,4 +1,4 @@
-package com.okta.springboot.simpleapp;
+package com.okta.springbootwithauth;
 
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.context.annotation.Configuration;
@@ -6,17 +6,11 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-@EnableOAuth2Sso
 @Configuration
-@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SpringSecurityWebAppConfig extends WebSecurityConfigurerAdapter {
     
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                .antMatchers("/").permitAll()
-                .antMatchers("/img/**").permitAll()
-                .anyRequest().authenticated();
     }
     
 }
