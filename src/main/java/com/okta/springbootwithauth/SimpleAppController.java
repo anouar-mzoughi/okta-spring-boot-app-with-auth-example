51f8c421c929bfeb1010769059f538b683caa2bf
Andrew Hughes <andrewcarterhughes@gmail.com>
2018-10-02 12:30:03 -0700

    okta oauth

--- src/main/java/com/okta/springbootwithauth/SimpleAppController.java
+++ src/main/java/com/okta/springbootwithauth/SimpleAppController.java
@@ -10,5 +10,10 @@ class SimpleAppController {
     String home() {
         return "home";
     }
+    
+    @RequestMapping("/restricted")
+    String restricted() {
+        return "restricted";
+    }
 
 }
--- src/main/java/com/okta/springbootwithauth/SpringSecurityWebAppConfig.java
+++ src/main/java/com/okta/springbootwithauth/SpringSecurityWebAppConfig.java
@@ -6,11 +6,16 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+@EnableOAuth2Sso
 @Configuration
 public class SpringSecurityWebAppConfig extends WebSecurityConfigurerAdapter {
-    
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/").permitAll()
+                .antMatchers("/img/**").permitAll()
+                .anyRequest().authenticated();
     }
     
 }
