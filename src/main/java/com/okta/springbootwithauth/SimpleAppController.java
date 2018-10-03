5de190ae7015378746813a2914150c68c153b3c6
Andrew Hughes <andrewcarterhughes@gmail.com>
2018-10-03 10:03:35 -0700

    group auth working

--- src/main/java/com/okta/springbootwithauth/SimpleAppController.java
+++ src/main/java/com/okta/springbootwithauth/SimpleAppController.java
@@ -1,5 +1,6 @@
 package com.okta.springbootwithauth;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -14,6 +15,15 @@ class SimpleAppController {
     @RequestMapping("/restricted")
     String restricted() {
         return "restricted";
+    }
+
+    @Autowired
+    AdminService adminService;
+
+    @RequestMapping("/admin")
+    String admin() {
+        adminService.ensureAdmin();
+        return "admin";
     }
 
 }
